72164f6af0bdc93f8781f1c3d43f93a7775986b2
Johann Neuhauser <johann@it-neuhauser.de>
2019-05-14 15:16:57 +0200

    Add app icons, gitea logo as vector graphic and login view with controller as dummy

---
+++ Gitea/ViewControllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Gitea
+//
+//  Created by Johann Neuhauser on 14.05.19.
+//  Copyright © 2019 Johann Neuhauser. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
