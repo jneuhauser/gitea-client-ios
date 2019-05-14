ce26ae55a6b21ad641431a7c1a308d73f80b1175
Johann Neuhauser <johann@it-neuhauser.de>
2019-05-14 15:56:42 +0200

    Add unconstrained ui elements and preliminary handling code for login view

--- Gitea/ViewControllers/LoginViewController.swift
+++ Gitea/ViewControllers/LoginViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var serverTextField: UITextField!
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        serverTextField.delegate = self
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +32,24 @@ class LoginViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        switch textField {
+        case serverTextField:
+            // If we have the server text field and the result is nil the regex didn´t match and the input is not allowed
+            if string.range(of: "^(https:\\/\\/)[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$", options: .regularExpression) == nil {
+                return false
+            }
+        default:
+            debugPrint("LoginViewController.textField(): Triggered by unhandled UITextField")
+            break
+        }
+        
+        return true
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("login buton pressed")
+    }
+    
 }
