8d0602f32df775cf60a67abcc4a071ff9aa97a4a
Johann Neuhauser <johann@it-neuhauser.de>
2019-06-10 10:05:29 +0200

    Add octicons and example implementation for code/folder/file table view

--- Gitea/ViewControllers/CodeTableViewController.swift
+++ Gitea/ViewControllers/CodeTableViewController.swift
@@ -39,23 +39,23 @@ class CodeTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CodeCell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = "Text Label"
+        cell.detailTextLabel?.text = "Detail Label"
+        cell.imageView?.image = UIImage(named: "file")
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
