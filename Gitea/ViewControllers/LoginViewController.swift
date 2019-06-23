ca0a41c5becbd1694faf382ee6876fdd7a062fae
Johann Neuhauser <johann@it-neuhauser.de>
2019-06-23 17:17:20 +0200

    Remove my test server url

--- Gitea/ViewControllers/LoginViewController.swift
+++ Gitea/ViewControllers/LoginViewController.swift
@@ -26,8 +26,6 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         userTextField.delegate = self
         passwordTextField.delegate = self
 
-        // Do any additional setup after loading the view.
-        // serverTextField.text = "https://git.it-neuhauser.de"
         serverTextField.text = "https://try.gitea.io"
         userTextField.text = "devel"
         passwordTextField.text = "DasIstEinTestUser"
