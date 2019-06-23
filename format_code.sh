ea3df009dd82267c1b3d44da76cd7ebe07f0bc32
Johann Neuhauser <johann@it-neuhauser.de>
2019-06-23 14:19:38 +0200

    Add swiftformat config and helper script

---
+++ format_code.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Use this script to fomat code with commandline or ceckout
+# https://github.com/nicklockwood/SwiftFormat#xcode-build-phase
+# for XCode integration
+
+./Pods/SwiftFormat/CommandLineTool/swiftformat ./Gitea/
